fbc236703fc9c062cdf82f32239d96d47a574685
Brandon Campbell Carranza <brandoncampbellcarranza01@gmail.com>
2019-11-15 21:02:00 -0800

    undid schema

--- server-express-mongodb/models/customer.js
+++ server-express-mongodb/models/customer.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CustomerSchema = new Schema({
-  id: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: Number, required: true}
